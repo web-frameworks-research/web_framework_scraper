762256816a0696ee194e5cffb69e4fc9b58cf76c
Om Duggineni <omduggineni@gmail.com>
2024-02-21 23:09:21 -0500

    made scraper robust to exceptions

--- throttle_function_call.mjs
+++ throttle_function_call.mjs
@@ -1,28 +1,42 @@
 function throttleFunctionCall(fn, num_concurrent) {
     let queue = [];
     let running = 0;
+
+    function try_run_queue() {
+        if (queue.length > 0) {
+            (queue.pop())();
+        }
+    }
     
     return async (...args) => {
         if (running < num_concurrent) {
             running++;
-            let value = await fn(...args);
-            running--;
-            if(queue.length > 0) {
-                let next = queue.pop();
-                next();
+            let value;
+            try {
+                value = await fn(...args);
+            } catch (e) {
+                running--;
+                try_run_queue();
+                throw e;
             }
+            running--;
+            try_run_queue();
             return value;
         } else {
             await new Promise((resolve, reject) => {
                 queue.push(resolve);
             });
             running++;
-            let value = await fn(...args);
-            running--;
-            if(queue.length > 0) {
-                let next = queue.pop();
-                next();
+            let value;
+            try {
+                value = await fn(...args);
+            } catch (e) {
+                running--;
+                try_run_queue();
+                throw e;
             }
+            running--;
+            try_run_queue();
             return value;
         }
     };
